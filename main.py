de80b8c4d3d17e0fb10889c77bc04dc19c71c66f
AmirKSalah <57016382+AmirKSalah@users.noreply.github.com>
2023-08-07 12:49:38 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+browser = webdriver.Chrome()
+url = 'https://letterboxd.com/films/popular/'
+browser.get(url)
+html = browser.page_source
+soup = BeautifulSoup(html, features='html.parser')
+results = soup.find(id="films-browser-list-container")
+films = results.find_all("a")
+print(films)
+i = 1
+for film in films:
+    name = film.find("span", class_="frame-title")
+    print(str(i) + "." + name.text)
+    i += 1
+    if i > 50:
+        break
